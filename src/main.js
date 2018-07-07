e7e6e3d9cbdc2037a0065bc72c17e4f00e446c6f
andgordio <andgordio@gmail.com>
2018-07-07 12:24:52 +0300

    D1 part III

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: 'UA-121940961-1'
+})
 
 new Vue({
   router,
